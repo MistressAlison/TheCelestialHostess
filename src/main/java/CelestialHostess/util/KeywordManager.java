306fcb389e2dea0cf2113bceef1d23b071700df4
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-10-08 09:02:52 -0300

    Add Combo keyword

--- src/main/java/CelestialHostess/util/KeywordManager.java
+++ src/main/java/CelestialHostess/util/KeywordManager.java
@@ -12,6 +12,7 @@ public class KeywordManager {
     public static String INFUSE;
     public static String GRAB;
     public static String GUIDE;
+    public static String COMBO;
 
     public static String getKeyword(String ID) {
         return keywordMap.getOrDefault(ID, "");

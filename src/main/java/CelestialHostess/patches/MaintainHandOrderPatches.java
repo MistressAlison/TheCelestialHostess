e90cf1dad0a8c388a039b2dce1c8dcabbdd31b1a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-10-10 11:34:33 -0300

    Add HandOrder patch

---
+++ src/main/java/CelestialHostess/patches/MaintainHandOrderPatches.java
@@ -0,0 +1,38 @@
+package CelestialHostess.patches;
+
+import CelestialHostess.TheCelestialHostess;
+import CelestialHostess.util.Wiz;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.screens.select.HandCardSelectScreen;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class MaintainHandOrderPatches {
+    private static final ArrayList<AbstractCard> cardOrder = new ArrayList<>();
+    @SpirePatch2(clz = HandCardSelectScreen.class, method = "prep")
+    public static class SaveOrder {
+        @SpirePostfixPatch
+        public static void save() {
+            if (Wiz.adp() instanceof TheCelestialHostess) {
+                cardOrder.clear();
+                cardOrder.addAll(Wiz.adp().hand.group);
+            }
+        }
+    }
+
+    @SpirePatch2(clz = HandCardSelectScreen.class, method = "refreshSelectedCards")
+    public static class LoadOrder {
+        @SpirePostfixPatch
+        public static void load() {
+            if (Wiz.adp() instanceof TheCelestialHostess) {
+                ArrayList<AbstractCard> newCards = cardOrder.stream().filter(Wiz.adp().hand::contains).collect(Collectors.toCollection(ArrayList::new));
+                Wiz.adp().hand.group.removeAll(newCards);
+                Wiz.adp().hand.group.addAll(newCards);
+                Wiz.adp().hand.refreshHandLayout();
+            }
+        }
+    }
+}

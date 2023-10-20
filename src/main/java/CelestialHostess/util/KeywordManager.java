23905a84ba901d5dc08903c90bc733723d62a94b
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-10-20 19:50:40 -0300

    Add Ability to KeywordManager

--- src/main/java/CelestialHostess/util/KeywordManager.java
+++ src/main/java/CelestialHostess/util/KeywordManager.java
@@ -14,6 +14,7 @@ public class KeywordManager {
     public static String GUIDE;
     public static String COMBO;
     public static String ABILITY;
+    public static String PIETY;
 
     public static String getKeyword(String ID) {
         return keywordMap.getOrDefault(ID, "");

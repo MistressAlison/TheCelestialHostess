7c719337d89df81be81229faa73ae25967d01623
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-10-30 08:38:38 -0300

    Remove unused tags

--- src/main/java/CelestialHostess/patches/CustomTags.java
+++ src/main/java/CelestialHostess/patches/CustomTags.java
@@ -6,8 +6,4 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 public class CustomTags {
     @SpireEnum
     public static AbstractCard.CardTags HOSTESS_HOLY;
-    @SpireEnum
-    public static AbstractCard.CardTags HOSTESS_GRAB;
-    @SpireEnum
-    public static AbstractCard.CardTags HOSTESS_COMBO;
 }

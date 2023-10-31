f9db679ff443d7e7bde286fb7afd3caa4a168f79
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-10-31 11:23:04 -0300

    Clean imports

--- src/main/java/CelestialHostess/actions/TributeAction.java
+++ src/main/java/CelestialHostess/actions/TributeAction.java
@@ -4,9 +4,6 @@ import CelestialHostess.util.Wiz;
 import com.megacrit.cardcrawl.actions.common.ExhaustAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
-import java.util.List;
-import java.util.function.Consumer;
-
 public class TributeAction extends BetterSelectCardsInHandAction {
     public TributeAction(Runnable doIf) {
         super(1, ExhaustAction.TEXT[0], false, false, c -> true, l -> {

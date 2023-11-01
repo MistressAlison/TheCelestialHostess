01597e524e6622d56623ab079a3e267ad931504e
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-01 12:23:35 -0300

    Add TempleWithin, Venerate

---
+++ src/main/java/CelestialHostess/cards/TempleWithin.java
@@ -0,0 +1,37 @@
+package CelestialHostess.cards;
+
+import CelestialHostess.actions.CleansePowerAction;
+import CelestialHostess.cards.abstracts.AbstractEasyCard;
+import com.megacrit.cardcrawl.cards.purple.EmptyBody;
+import com.megacrit.cardcrawl.cards.purple.Sanctity;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import static CelestialHostess.MainModfile.makeID;
+
+
+public class TempleWithin extends AbstractEasyCard {
+    public final static String ID = makeID(TempleWithin.class.getSimpleName());
+
+    public TempleWithin() {
+        super(ID, 2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseBlock = block = 11;
+        baseMagicNumber = magicNumber = 1;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+        addToBot(new CleansePowerAction(p, magicNumber, pow -> pow.type == AbstractPower.PowerType.DEBUFF));
+    }
+
+    public void upp() {
+        upgradeBlock(4);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return EmptyBody.ID;
+    }
+}
---
+++ src/main/java/CelestialHostess/cards/Venerate.java
@@ -0,0 +1,37 @@
+package CelestialHostess.cards;
+
+import CelestialHostess.cards.abstracts.AbstractEasyCard;
+import CelestialHostess.powers.CardToHandPower;
+import CelestialHostess.util.Wiz;
+import com.megacrit.cardcrawl.cards.purple.Sanctity;
+import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static CelestialHostess.MainModfile.makeID;
+
+
+public class Venerate extends AbstractEasyCard {
+    public final static String ID = makeID(Venerate.class.getSimpleName());
+
+    public Venerate() {
+        super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.NONE);
+        baseBlock = block = 7;
+        baseMagicNumber = magicNumber = 1;
+        cardsToPreview = new Miracle();
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+        Wiz.applyToSelf(new CardToHandPower(p, magicNumber, new Miracle()));
+    }
+
+    public void upp() {
+        upgradeBlock(3);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Sanctity.ID;
+    }
+}

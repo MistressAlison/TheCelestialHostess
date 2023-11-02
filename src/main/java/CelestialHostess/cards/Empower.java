6cc9430a2609106dc3cd881f970cb8a7965a3d53
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-02 17:32:15 -0300

    Add Empower

---
+++ src/main/java/CelestialHostess/cards/Empower.java
@@ -0,0 +1,35 @@
+package CelestialHostess.cards;
+
+import CelestialHostess.actions.TributeAction;
+import CelestialHostess.cards.abstracts.AbstractEasyCard;
+import CelestialHostess.powers.PietyPower;
+import CelestialHostess.util.Wiz;
+import com.megacrit.cardcrawl.cards.purple.Crescendo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static CelestialHostess.MainModfile.makeID;
+
+public class Empower extends AbstractEasyCard {
+    public final static String ID = makeID(Empower.class.getSimpleName());
+
+    public Empower() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseMagicNumber = magicNumber = 3;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new TributeAction(() -> Wiz.applyToSelfTop(new PietyPower(p, magicNumber))));
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Crescendo.ID;
+    }
+}

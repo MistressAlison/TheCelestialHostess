feceafe724962701b2b03480d0f61bae77d01c19
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-02 17:35:45 -0300

    Add MagicWall

---
+++ src/main/java/CelestialHostess/cards/MagicWall.java
@@ -0,0 +1,38 @@
+package CelestialHostess.cards;
+
+import CelestialHostess.cards.abstracts.AbstractEasyCard;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.blue.ForceField;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.LoseDexterityPower;
+
+import static CelestialHostess.MainModfile.makeID;
+
+
+public class MagicWall extends AbstractEasyCard {
+    public final static String ID = makeID(MagicWall.class.getSimpleName());
+
+    public MagicWall() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseBlock = block = 7;
+        baseMagicNumber = magicNumber = 2;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        blck();
+        addToBot(new ApplyPowerAction(p, p, new DexterityPower(p, magicNumber)));
+        addToBot(new ApplyPowerAction(p, p, new LoseDexterityPower(p, magicNumber)));
+    }
+
+    public void upp() {
+        upgradeBlock(2);
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return ForceField.ID;
+    }
+}

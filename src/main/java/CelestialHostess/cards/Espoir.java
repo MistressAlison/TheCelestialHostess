5f49556daff9a9ef892876117131d16f96cbd564
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-01 22:33:22 -0300

    Temple Within now Espoir

--- src/main/java/CelestialHostess/cards/Espoir.java
+++ src/main/java/CelestialHostess/cards/Espoir.java
@@ -2,7 +2,7 @@ package CelestialHostess.cards;
 
 import CelestialHostess.actions.CleansePowerAction;
 import CelestialHostess.cards.abstracts.AbstractEasyCard;
-import com.megacrit.cardcrawl.cards.purple.EmptyBody;
+import com.megacrit.cardcrawl.cards.purple.Pray;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
@@ -10,10 +10,10 @@ import com.megacrit.cardcrawl.powers.AbstractPower;
 import static CelestialHostess.MainModfile.makeID;
 
 
-public class TempleWithin extends AbstractEasyCard {
-    public final static String ID = makeID(TempleWithin.class.getSimpleName());
+public class Espoir extends AbstractEasyCard {
+    public final static String ID = makeID(Espoir.class.getSimpleName());
 
-    public TempleWithin() {
+    public Espoir() {
         super(ID, 2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
         baseBlock = block = 11;
         baseMagicNumber = magicNumber = 1;
@@ -30,6 +30,6 @@ public class TempleWithin extends AbstractEasyCard {
 
     @Override
     public String cardArtCopy() {
-        return EmptyBody.ID;
+        return Pray.ID;
     }
 }

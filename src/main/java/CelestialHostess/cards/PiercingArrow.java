e875d3bcfc28730811be7b037be9a6d8c47356ae
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-03 19:38:53 -0300

    Piercing Arrow now X cost Uncommon

--- src/main/java/CelestialHostess/cards/PiercingArrow.java
+++ src/main/java/CelestialHostess/cards/PiercingArrow.java
@@ -1,9 +1,9 @@
 package CelestialHostess.cards;
 
+import CelestialHostess.actions.EasyXCostAction;
 import CelestialHostess.actions.HolyAction;
 import CelestialHostess.cards.abstracts.AbstractEasyCard;
 import CelestialHostess.patches.CustomTags;
-import CelestialHostess.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -17,19 +17,29 @@ public class PiercingArrow extends AbstractEasyCard {
     public final static String ID = makeID(PiercingArrow.class.getSimpleName());
 
     public PiercingArrow() {
-        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 6;
+        super(ID, -1, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = damage = 4;
         tags.add(CustomTags.HOSTESS_HOLY);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new HolyAction(() -> Wiz.att(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE, true))));
+        addToBot(new HolyAction(() -> addToTop(new EasyXCostAction(this, (base, params) -> {
+            int effect = base;
+            for (int i : params) {
+                effect += i;
+            }
+            for (int i = 0 ; i < effect ; i++) {
+                addToTop(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE, true));
+            }
+            return true;
+        }))));
+        //addToBot(new HolyAction(() -> Wiz.att(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE, true))));
     }
 
     @Override
     public void upp() {
-        upgradeDamage(3);
+        upgradeDamage(2);
     }
 
     @Override

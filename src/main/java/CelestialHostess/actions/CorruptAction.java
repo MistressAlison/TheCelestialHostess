291d7cd21d7ee6703b0a4ec3ac1d059b4bdbc1d1
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-06 11:26:38 -0400

    Add SFX and VFX to CorruptAction

--- src/main/java/CelestialHostess/actions/CorruptAction.java
+++ src/main/java/CelestialHostess/actions/CorruptAction.java
@@ -6,10 +6,15 @@ import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.status.VoidCard;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.combat.DarkOrbActivateEffect;
 
 public class CorruptAction extends DoAction {
     public CorruptAction() {
         super(() -> {
+            CardCrawlGame.sound.play("ORB_DARK_EVOKE", 0.1F);// 74
+            AbstractDungeon.effectsQueue.add(new DarkOrbActivateEffect(Wiz.adp().hb.cX, Wiz.adp().hb.cY));
             for (AbstractCard c : Wiz.adp().hand.group) {
                 if (c instanceof Miracle) {
                     Wiz.att(new ExhaustSpecificCardAction(c, Wiz.adp().hand));
--- src/main/java/CelestialHostess/cards/VoidSpirit.java
+++ src/main/java/CelestialHostess/cards/VoidSpirit.java
@@ -4,12 +4,9 @@ import CelestialHostess.actions.CorruptAction;
 import CelestialHostess.cards.abstracts.AbstractEasyCard;
 import CelestialHostess.powers.PurityPower;
 import CelestialHostess.util.Wiz;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.red.GhostlyArmor;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.vfx.combat.DarkOrbActivateEffect;
 
 import static CelestialHostess.MainModfile.makeID;
 
@@ -23,8 +20,6 @@ public class VoidSpirit extends AbstractEasyCard {
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new SFXAction("ORB_DARK_EVOKE", 0.1F));
-        addToBot(new VFXAction(new DarkOrbActivateEffect(p.hb.cX, p.hb.cY)));
         Wiz.applyToSelf(new PurityPower(p, magicNumber));
         addToBot(new CorruptAction());
     }

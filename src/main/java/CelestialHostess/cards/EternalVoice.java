5820ef6adc68ec8d96c3deb60c52db4a79f1b21d
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-26 22:29:13 -0400

    Rework EternalVoice, GearRepair

--- src/main/java/CelestialHostess/cards/EternalVoice.java
+++ src/main/java/CelestialHostess/cards/EternalVoice.java
@@ -1,44 +1,38 @@
 package CelestialHostess.cards;
 
-import CelestialHostess.actions.HolyAction;
-import CelestialHostess.cards.abstracts.AbstractEasyCard;
-import CelestialHostess.patches.CustomTags;
-import CelestialHostess.util.Wiz;
+import CelestialHostess.cards.abstracts.AbstractEmpowerCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.green.PiercingWail;
-import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.combat.IntimidateEffect;
 
 import static CelestialHostess.MainModfile.makeID;
 
-public class EternalVoice extends AbstractEasyCard {
+public class EternalVoice extends AbstractEmpowerCard {
     public final static String ID = makeID(EternalVoice.class.getSimpleName());
 
     public EternalVoice() {
         super(ID, 0, CardType.ATTACK, CardRarity.COMMON, CardTarget.ALL_ENEMY);
         baseDamage = damage = 3;
+        baseMagicNumber = magicNumber = 1;
         isMultiDamage = true;
-        tags.add(CustomTags.HOSTESS_IF_MIRACLE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new SFXAction("ATTACK_PIERCING_WAIL"));
         addToBot(new VFXAction(p, new IntimidateEffect(p.hb.cX, p.hb.cY), 1.0F));
-        allDmg(AbstractGameAction.AttackEffect.FIRE, true);
-        addToBot(new HolyAction(() -> allDmg(AbstractGameAction.AttackEffect.FIRE)));
+        for (int i = 0 ; i < magicNumber ; i++) {
+            allDmg(AbstractGameAction.AttackEffect.FIRE, true);
+        }
     }
 
-    public void triggerOnGlowCheck() {
-        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
-        if (Wiz.adp().hand.group.stream().anyMatch(c -> c instanceof Miracle)) {
-            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
-        }
+    @Override
+    public void onEmpower() {
+        upgradeMagicNumber(1);
     }
 
     @Override
--- src/main/java/CelestialHostess/cards/GearRepair.java
+++ src/main/java/CelestialHostess/cards/GearRepair.java
@@ -1,12 +1,8 @@
 package CelestialHostess.cards;
 
 import CelestialHostess.actions.TributeAction;
-import CelestialHostess.cardmods.FlatBlockMod;
 import CelestialHostess.cards.abstracts.AbstractEasyCard;
-import CelestialHostess.util.Wiz;
-import basemod.helpers.CardModifierManager;
-import com.badlogic.gdx.graphics.Color;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.actions.unique.ArmamentsAction;
 import com.megacrit.cardcrawl.cards.red.Armaments;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
@@ -18,24 +14,17 @@ public class GearRepair extends AbstractEasyCard {
 
     public GearRepair() {
         super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.SELF);
-        baseMagicNumber = magicNumber = 3;
+        exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new TributeAction(() -> {
-            for (AbstractCard c : Wiz.adp().hand.group) {
-                if (c.baseBlock > 0) {
-                    c.superFlash(Color.GOLD.cpy());
-                    CardModifierManager.addModifier(c, new FlatBlockMod(magicNumber));
-                }
-            }
-        }));
+        addToBot(new TributeAction(() -> addToTop(new ArmamentsAction(true))));
     }
 
     @Override
     public void upp() {
-        upgradeMagicNumber(2);
+        upgradeBaseCost(0);
     }
 
     @Override

9cf371ea2cfa5efc79ac073b795bbbbd0866b810
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-28 13:36:41 -0400

    CrossCut now Cleave instead of TwinStrike

--- src/main/java/CelestialHostess/cards/CrossCut.java
+++ src/main/java/CelestialHostess/cards/CrossCut.java
@@ -3,10 +3,13 @@ package CelestialHostess.cards;
 import CelestialHostess.cards.abstracts.AbstractEasyCard;
 import CelestialHostess.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.purple.FollowUp;
 import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.CleaveEffect;
 
 import static CelestialHostess.MainModfile.makeID;
 
@@ -14,22 +17,24 @@ public class CrossCut extends AbstractEasyCard {
     public final static String ID = makeID(CrossCut.class.getSimpleName());
 
     public CrossCut() {
-        super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 5;
+        super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ALL_ENEMY);
+        baseDamage = damage = 8;
         baseMagicNumber = magicNumber = 1;
+        isMultiDamage = true;
         cardsToPreview = new Miracle();
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
-        dmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
+        addToBot(new SFXAction("ATTACK_HEAVY"));
+        addToBot(new VFXAction(p, new CleaveEffect(), 0.1F));
+        allDmg(AbstractGameAction.AttackEffect.NONE);
         Wiz.makeInHand(new Miracle(), magicNumber);
     }
 
     @Override
     public void upp() {
-        upgradeDamage(2);
+        upgradeDamage(3);
     }
 
     @Override

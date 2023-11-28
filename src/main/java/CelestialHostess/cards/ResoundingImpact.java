fa44cb9470877f9bcf62d15833c5be6037245459
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-28 07:18:36 -0400

    Remove Holy condition from ResoundingImpact. Buff damage 10 -> 12.

--- src/main/java/CelestialHostess/cards/ResoundingImpact.java
+++ src/main/java/CelestialHostess/cards/ResoundingImpact.java
@@ -1,16 +1,12 @@
 package CelestialHostess.cards;
 
-import CelestialHostess.actions.HolyAction;
+import CelestialHostess.actions.DoAction;
 import CelestialHostess.cardmods.FlatDamageMod;
 import CelestialHostess.cards.abstracts.AbstractEasyCard;
-import CelestialHostess.patches.CustomTags;
-import CelestialHostess.util.Wiz;
 import basemod.helpers.CardModifierManager;
 import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.red.Bash;
-import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -21,25 +17,17 @@ public class ResoundingImpact extends AbstractEasyCard {
 
     public ResoundingImpact() {
         super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = damage = 10;
+        baseDamage = damage = 12;
         baseMagicNumber = magicNumber = 6;
-        tags.add(CustomTags.HOSTESS_IF_MIRACLE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
-        addToBot(new HolyAction(() -> {
+        addToBot(new DoAction(() -> {
             superFlash(Color.GOLD.cpy());
             CardModifierManager.addModifier(this, new FlatDamageMod(magicNumber));
         }));
-    }
-
-    public void triggerOnGlowCheck() {
-        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
-        if (Wiz.adp().hand.group.stream().anyMatch(c -> c instanceof Miracle)) {
-            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
-        }
     }
 
     @Override

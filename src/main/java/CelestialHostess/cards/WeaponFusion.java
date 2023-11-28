c92f0561246b7a56eee60e13dfa1898609c4c9ad
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-27 22:52:19 -0400

    Reintroduce WeaponFusion, now adds a Strike

--- src/main/java/CelestialHostess/cards/WeaponFusion.java
+++ src/main/java/CelestialHostess/cards/WeaponFusion.java
@@ -1,8 +1,11 @@
-package CelestialHostess.cutStuff.cards;
+package CelestialHostess.cards;
 
 import CelestialHostess.actions.BetterSelectCardsInHandAction;
+import CelestialHostess.cardmods.FlatDamageMod;
 import CelestialHostess.cards.abstracts.AbstractEasyCard;
 import CelestialHostess.util.Wiz;
+import basemod.abstracts.AbstractCardModifier;
+import basemod.helpers.CardModifierManager;
 import com.megacrit.cardcrawl.actions.common.ExhaustAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.purple.ConjureBlade;
@@ -18,17 +21,20 @@ public class WeaponFusion extends AbstractEasyCard {
     public WeaponFusion() {
         super(ID, 1, CardType.SKILL, CardRarity.RARE, CardTarget.NONE);
         baseMagicNumber = magicNumber = 2;
-        cardsToPreview = new FlameBlessing();
+        cardsToPreview = new Strike();
         exhaust = true;
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new BetterSelectCardsInHandAction(2, ExhaustAction.TEXT[0], true, true, c -> c.type == CardType.ATTACK, l -> {
-            AbstractCard card = new FlameBlessing();
+            AbstractCard card = new Strike();
             card.baseDamage = 0;
             for (AbstractCard c : l) {
                 Wiz.adp().hand.moveToExhaustPile(c);
                 card.baseDamage += c.baseDamage;
+                for (AbstractCardModifier mod : CardModifierManager.getModifiers(c, FlatDamageMod.ID)) {
+                    CardModifierManager.addModifier(card, mod.makeCopy());
+                }
             }
             Wiz.makeInHand(card);
             l.clear();

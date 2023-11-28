1d2041023c2f54e7160ef70c5a71bb153253e80a
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-28 09:47:53 -0400

    Add StrikeBuffPower

---
+++ src/main/java/CelestialHostess/powers/StrikeBuffPower.java
@@ -0,0 +1,46 @@
+package CelestialHostess.powers;
+
+import CelestialHostess.MainModfile;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class StrikeBuffPower extends AbstractPower {
+    public static final String POWER_ID = MainModfile.makeID(StrikeBuffPower.class.getSimpleName());
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public StrikeBuffPower(AbstractCreature owner, int amount) {
+        this.ID = POWER_ID;
+        this.name = NAME;
+        this.owner = owner;
+        this.amount = amount;
+        this.type = PowerType.BUFF;
+        this.isTurnBased = false;
+        this.loadRegion("accuracy");
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1];
+    }
+
+    @Override
+    public float atDamageGive(float damage, DamageInfo.DamageType type, AbstractCard card) {
+        if (type == DamageInfo.DamageType.NORMAL && card.hasTag(AbstractCard.CardTags.STRIKE)) {
+            return damage + amount;
+        }
+        return damage;
+    }
+
+    @Override
+    public void atEndOfRound() {
+        addToBot(new RemoveSpecificPowerAction(owner, owner, this));
+    }
+}

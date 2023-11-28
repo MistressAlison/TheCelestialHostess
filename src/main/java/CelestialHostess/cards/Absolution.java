fe2d49ffc008b41041f92b831dd75b76a24be3fe
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-28 19:18:44 -0400

    Absolution nerf 4(6) -> 3(4)

--- src/main/java/CelestialHostess/cards/Absolution.java
+++ src/main/java/CelestialHostess/cards/Absolution.java
@@ -14,7 +14,7 @@ public class Absolution extends AbstractEasyCard {
 
     public Absolution() {
         super(ID, 1, CardType.POWER, CardRarity.UNCOMMON, CardTarget.SELF);
-        baseMagicNumber = magicNumber = 4;
+        baseMagicNumber = magicNumber = 3;
     }
 
     @Override
@@ -24,7 +24,7 @@ public class Absolution extends AbstractEasyCard {
 
     @Override
     public void upp() {
-        upgradeMagicNumber(2);
+        upgradeMagicNumber(1);
     }
 
     @Override

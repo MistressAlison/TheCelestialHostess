883d4bf56bce02da0aba8357df01c9e9085a84d5
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-27 22:51:33 -0400

    Add support for powers making cards free to play

---
+++ src/main/java/CelestialHostess/patches/FreeCardPatch.java
@@ -0,0 +1,28 @@
+package CelestialHostess.patches;
+
+import CelestialHostess.powers.interfaces.FreeToPlayPower;
+import CelestialHostess.util.Wiz;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch2;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+public class FreeCardPatch {
+    @SpirePatch2(clz = AbstractCard.class, method = "freeToPlay")
+    public static class FreeCardPlz {
+        @SpirePrefixPatch
+        public static SpireReturn<?> free(AbstractCard __instance) {
+            if (Wiz.adp() != null && !AbstractDungeon.isScreenUp && AbstractDungeon.currMapNode != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
+                for (AbstractPower pow : Wiz.adp().powers) {
+                    if (pow instanceof FreeToPlayPower && ((FreeToPlayPower) pow).isFreeToPlay(__instance)) {
+                        return SpireReturn.Return(true);
+                    }
+                }
+            }
+            return SpireReturn.Continue();
+        }
+    }
+}
---
+++ src/main/java/CelestialHostess/powers/interfaces/FreeToPlayPower.java
@@ -0,0 +1,7 @@
+package CelestialHostess.powers.interfaces;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public interface FreeToPlayPower {
+    boolean isFreeToPlay(AbstractCard card);
+}

79d0499272721c9494f793fad02066018328d0c2
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-11-28 07:21:54 -0400

    Lightmare reworked. Now gives Light Charge and Corrupts unconditionally.

--- src/main/java/CelestialHostess/cards/Lightmare.java
+++ src/main/java/CelestialHostess/cards/Lightmare.java
@@ -1,19 +1,12 @@
 package CelestialHostess.cards;
 
 import CelestialHostess.actions.CorruptAction;
-import CelestialHostess.actions.HolyAction;
-import CelestialHostess.actions.ProcessDamageRandomEnemyAction;
 import CelestialHostess.cards.abstracts.AbstractEasyCard;
-import CelestialHostess.patches.CustomTags;
-import CelestialHostess.vfx.BigExplosionVFX;
-import CelestialHostess.vfx.SpotlightEnemyEffect;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
+import CelestialHostess.powers.LightChargePower;
+import CelestialHostess.util.Wiz;
 import com.megacrit.cardcrawl.cards.blue.Storm;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.vfx.combat.InversionBeamEffect;
 
 import static CelestialHostess.MainModfile.makeID;
 
@@ -21,25 +14,20 @@ public class Lightmare extends AbstractEasyCard {
     public final static String ID = makeID(Lightmare.class.getSimpleName());
 
     public Lightmare() {
-        super(ID, 0, CardType.ATTACK, CardRarity.RARE, CardTarget.ALL_ENEMY);
-        baseDamage = damage = 15;
-        tags.add(CustomTags.HOSTESS_IF_MIRACLE);
+        super(ID, 1, CardType.SKILL, CardRarity.RARE, CardTarget.NONE);
+        baseMagicNumber = magicNumber = 1;
+        exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ProcessDamageRandomEnemyAction(this, (mon, total) -> {
-            addToTop(new BigExplosionVFX(mon));
-            addToTop(new DamageAction(mon, new DamageInfo(p, total, damageTypeForTurn)));
-            addToTop(new VFXAction(new InversionBeamEffect(mon.hb.cX)));
-            addToTop(new VFXAction(new SpotlightEnemyEffect(mon)));
-        }));
-        addToBot(new HolyAction(() -> addToTop(new CorruptAction())));
+        Wiz.applyToSelf(new LightChargePower(p, magicNumber));
+        addToBot(new CorruptAction());
     }
 
     @Override
     public void upp() {
-        upgradeDamage(5);
+        upgradeMagicNumber(1);
     }
 
     @Override

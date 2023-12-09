6bb26877cdb03f924637f29e5c5a334565bf548c
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-12-08 20:11:14 -0400

    Blessed Weapon rework

--- src/main/java/CelestialHostess/cards/BlessedWeapon.java
+++ src/main/java/CelestialHostess/cards/BlessedWeapon.java
@@ -1,22 +1,15 @@
 package CelestialHostess.cards;
 
-import CelestialHostess.actions.DoAction;
-import CelestialHostess.actions.DoForEachMiracleAction;
-import CelestialHostess.cardmods.FlatDamageMod;
-import CelestialHostess.cards.abstracts.AbstractEasyCard;
 import CelestialHostess.cards.abstracts.AbstractEmpowerCard;
-import CelestialHostess.patches.CustomTags;
 import CelestialHostess.util.Wiz;
-import basemod.helpers.CardModifierManager;
-import com.badlogic.gdx.graphics.Color;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.red.Inflame;
-import com.megacrit.cardcrawl.cards.tempCards.Miracle;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.vfx.combat.CleaveEffect;
 
 import static CelestialHostess.MainModfile.makeID;
@@ -27,7 +20,8 @@ public class BlessedWeapon extends AbstractEmpowerCard {
     public BlessedWeapon() {
         super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
         baseDamage = damage = 11;
-        baseMagicNumber = magicNumber = 2;
+        baseMagicNumber = magicNumber = 1;
+        baseSecondMagic = secondMagic = 1;
         isMultiDamage = true;
     }
 
@@ -36,24 +30,32 @@ public class BlessedWeapon extends AbstractEmpowerCard {
         addToBot(new SFXAction("ATTACK_HEAVY"));
         addToBot(new VFXAction(p, new CleaveEffect(), 0.1F));
         allDmg(AbstractGameAction.AttackEffect.NONE);
-        addToBot(new DoAction(() -> {
+        Wiz.applyToSelf(new StrengthPower(p, magicNumber));
+        /*addToBot(new DoAction(() -> {
             for (AbstractCard c : Wiz.adp().hand.group) {
                 if (c.baseDamage > 0 && c.type == CardType.ATTACK) {
                     c.superFlash(Color.GOLD.cpy());
                     CardModifierManager.addModifier(c, new FlatDamageMod(magicNumber));
                 }
             }
-        }));
+        }));*/
+    }
+
+    @Override
+    public AbstractCard makeStatEquivalentCopy() {
+        AbstractCard copy = super.makeStatEquivalentCopy();
+        copy.magicNumber = this.magicNumber;
+        return copy;
     }
 
     @Override
     public void onEmpower() {
-        upgradeMagicNumber(1);
+        upgradeMagicNumber(secondMagic);
     }
 
     @Override
     public void upp() {
-        upgradeDamage(3);
+        upgradeDamage(2);
         upgradeMagicNumber(1);
     }
 

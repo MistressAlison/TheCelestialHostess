e3deffa78f55369563f26079f8de0804dbb32a6c
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2023-12-21 07:51:22 -0400

    Rework Imbued Strike to not cause infinites

--- src/main/java/CelestialHostess/cards/ImbuedStrike.java
+++ src/main/java/CelestialHostess/cards/ImbuedStrike.java
@@ -1,19 +1,12 @@
 package CelestialHostess.cards;
 
-import CelestialHostess.actions.DoIfAction;
-import CelestialHostess.cardmods.HolyMod;
 import CelestialHostess.cards.abstracts.AbstractEasyCard;
-import CelestialHostess.cards.abstracts.AbstractHolyInfoCard;
 import CelestialHostess.powers.ChargePreservationPower;
-import CelestialHostess.powers.interfaces.AuraTriggerPower;
 import CelestialHostess.util.Wiz;
-import basemod.helpers.CardModifierManager;
-import basemod.patches.com.megacrit.cardcrawl.cards.AbstractCard.MultiCardPreview;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.red.SeverSoul;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 
 import static CelestialHostess.MainModfile.makeID;
 
@@ -25,32 +18,17 @@ public class ImbuedStrike extends AbstractEasyCard {
         baseDamage = damage = 12;
         baseMagicNumber = magicNumber = 1;
         tags.add(CardTags.STRIKE);
-        CardModifierManager.addModifier(this, new HolyMod());
-        MultiCardPreview.add(this, new AbstractHolyInfoCard(this, cardStrings.EXTENDED_DESCRIPTION[0]) {});
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
-        addToBot(new DoIfAction(Wiz::auraActive,
-                () -> Wiz.applyToSelfTop(new ChargePreservationPower(p, magicNumber)),
-                () -> {
-                    for (AbstractPower pow : Wiz.adp().powers) {
-                        if (pow instanceof AuraTriggerPower) {
-                            ((AuraTriggerPower) pow).onActivateAura();
-                        }
-                    }
-                }));
+        Wiz.applyToSelf(new ChargePreservationPower(p, magicNumber));
     }
 
     @Override
     public void upp() {
         upgradeDamage(4);
-        MultiCardPreview.multiCardPreview.get(this).forEach(c -> {
-            if (c instanceof AbstractHolyInfoCard) {
-                c.upgrade();
-            }
-        });
     }
 
     @Override
